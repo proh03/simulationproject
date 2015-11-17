d316fbd89d96097927ca6ed6b03a7bd1264b5659
Kate Doughty <ksd4ww@virginia.edu>
2015-11-16 23:54:16 -0500

    meow

--- simulation/catGo.py
+++ simulation/catGo.py
@@ -1,10 +1,12 @@
-# from trepan.api import debug
+ # from trepan.api import debug
 
 # import pdb
 # pdb.set_trace()
 
 import runWorld as rw
 import drawWorld as dw
+import pygame as pg
+
 
 
 # Initialize world
@@ -25,9 +27,9 @@ def updateDisplay(state):
     dw.draw(myimage, (state, width/2))
 
 
-# We'll update the state on each tick by incrementing the x stateinate
+# We'll update the stnte on each tick by incrementing the x stateinate
 def updateState(state):
-    return(state+1)
+    return(state)
 
 # We'll terminate when the x stateinate reaches the screen edge
 
@@ -41,12 +43,25 @@ def endState(state):
 
 # For now we'll handle events just logging them to the console
 #
-def handleEvent(state, event):
-    return(state)
+#def handleEvent(state, event):
+   # return(state)
 
+def handleEvent(state, event):
+    if (event.type == pg.KEYDOWN):
+        if (event.key == pg.K_LEFT):
+            newstate = state - 10  
+            return (newstate)
+        if (event.key == pg.K_RIGHT):
+            newstate = state + 10
+            return (newstate)
+    else:
+        return state
+        
 # Off we go! Start the cat at the left edge, and try for 30 FPS
 frameRate = 60
 initState = 0
 # debug()
 rw.runWorld(initState, updateDisplay, updateState, handleEvent,
             endState, frameRate)
+
+
