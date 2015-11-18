4e0d77a0f75f6293d05194809d4d23dfecf4b0ad
Kate Doughty <ksd4ww@virginia.edu>
2015-11-18 15:29:07 -0500

    upanddown

--- simulation/catGo.py
+++ simulation/catGo.py
@@ -16,7 +16,7 @@ height = 500
 rw.newDisplay(width, height, name)
 
 # World state will be single x coordinate at left edge of world
-initState = 0
+
 
 # Display the state by drawing a cat at that x coordinate
 myimage = dw.loadImage("cat.bmp")
@@ -24,7 +24,7 @@ myimage = dw.loadImage("cat.bmp")
 
 def updateDisplay(state):
     dw.fill(dw.black)
-    dw.draw(myimage, (state, width/2))
+    dw.draw(myimage, state)
 
 
 # We'll update the stnte on each tick by incrementing the x stateinate
@@ -35,7 +35,7 @@ def updateState(state):
 
 
 def endState(state):
-    if (state >= width):
+    if (state[0] >= width) or (state[0] < 0) or (state[1] > height) or (state[1] < 0):
         return True
     else:
         return False
@@ -49,17 +49,23 @@ def endState(state):
 def handleEvent(state, event):
     if (event.type == pg.KEYDOWN):
         if (event.key == pg.K_LEFT):
-            newstate = state - 10  
+            newstate = (state[0] - 10,state[1])
             return (newstate)
         if (event.key == pg.K_RIGHT):
-            newstate = state + 10
+            newstate = (state [0] + 10, state[1])
+            return (newstate)
+        if (event.key == pg.K_UP):
+            newstate = (state [0], state [1] - 10)
+            return (newstate)
+        if (event.key == pg.K_DOWN):
+            newstate = (state [0], state [1] + 10)
             return (newstate)
     else:
         return state
         
 # Off we go! Start the cat at the left edge, and try for 30 FPS
 frameRate = 60
-initState = 0
+initState = (0,0)
 # debug()
 rw.runWorld(initState, updateDisplay, updateState, handleEvent,
             endState, frameRate)
